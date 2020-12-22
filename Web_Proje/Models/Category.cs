584def5fec02d2ea4296fb7d000cdb452205f682
Burak Can <57682748+BcanGRG@users.noreply.github.com>
2020-12-23 01:55:31 +0300

    commit

--- Web_Proje/Models/Category.cs
+++ Web_Proje/Models/Category.cs
@@ -10,5 +10,7 @@ namespace Web_Proje.Models
         public int CategoryID { get; set; }
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
+
+        public List <Food> Foods { get; set; }
     }
 }
