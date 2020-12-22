6fdda8ca506801a9812769664ee97546fb7922d5
Burak Can <57682748+BcanGRG@users.noreply.github.com>
2020-12-23 01:46:22 +0300

    yeni

--- Web_Proje/Models/Food.cs
+++ Web_Proje/Models/Food.cs
@@ -7,5 +7,15 @@ namespace Web_Proje.Models
 {
     public class Food
     {
+        public int FoodID { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string ImageURL { get; set; }
+        public string ThumbNailImageURL { get; set; }
+        public int Stock { get; set; }
+        public int Price { get; set; }
+
+
     }
 }
