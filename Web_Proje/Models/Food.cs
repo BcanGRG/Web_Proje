2b008236a484c5b8a84a3b037c70a3b5171c0703
Burak Can <57682748+BcanGRG@users.noreply.github.com>
2020-12-24 17:46:53 +0300

    Repository ve Baglanti

--- Web_Proje/Models/Food.cs
+++ Web_Proje/Models/Food.cs
@@ -9,10 +9,8 @@ namespace Web_Proje.Models
     {
         public int FoodID { get; set; }
         public string Name { get; set; }
-        public string ShortDescription { get; set; }
-        public string LongDescription { get; set; }
+        public string Description { get; set; }
         public string ImageURL { get; set; }
-        public string ThumbNailImageURL { get; set; }
         public int Stock { get; set; }
         public int Price { get; set; }
 
---
+++ Web_Proje/Repositories/CategoryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Proje.Models;
+
+namespace Web_Proje.Repositories
+{
+    public class CategoryRepository
+    {
+        Context c = new Context();
+
+        public List<Category> CategoryListele()
+        {
+            return c.Categories.ToList();
+        }
+
+        public void CategoryEkle(Category ct)
+        {
+            c.Categories.Add(ct);
+            c.SaveChanges();
+        }
+
+        public void CategorySil(Category ct)
+        {
+            c.Categories.Remove(ct);
+            c.SaveChanges();
+        }
+
+        public void CategoryGuncelle(Category ct)
+        {
+            c.Categories.Update(ct);
+            c.SaveChanges();
+        }
+
+        public void CategoryBul(int id)
+        {
+            c.Categories.Find(id);
+        }
+    }
+}
