5bff755da6fa56f0125a99fd4e69b0fac3626320
Burak Can <57682748+BcanGRG@users.noreply.github.com>
2020-12-25 16:31:35 +0300

    food

--- Web_Proje/Repositories/FoodRepository.cs
+++ Web_Proje/Repositories/FoodRepository.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Proje.Models;
 
 namespace Web_Proje.Repositories
 {
     public class FoodRepository
     {
+        Context c = new Context();
+
+        public List<Food> FoodListele()
+        {
+            return c.Foods.ToList();
+        }
+
+        public void FoodEkle(Food f)
+        {
+            c.Foods.Add(f);
+            c.SaveChanges();
+        }
+
+        public void FoodSil(Food f)
+        {
+            c.Foods.Remove(f);
+            c.SaveChanges();
+        }
+
+        public void FoodFuncelle(Food f)
+        {
+            c.Foods.Update(f);
+            c.SaveChanges();
+        }
+
+        public void FoodBul(int id)
+        {
+            c.Foods.Find(id);
+            c.SaveChanges();
+        }
     }
 }
