4bea5a5c94f91631df30effa9250ad29d3df3755
Burak Can <57682748+BcanGRG@users.noreply.github.com>
2020-12-28 21:43:10 +0300

    views ve layout

--- Web_Proje/Startup.cs
+++ Web_Proje/Startup.cs
@@ -33,7 +33,7 @@ namespace Web_Proje
             {
                 endpoints.MapControllerRoute(
                     name:"default",
-                    pattern : "{controller=CategoryController}/{action=Index}/{id?}"
+                    pattern : "{controller=Category}/{action=Index}/{id?}"
                     );
               
             });
